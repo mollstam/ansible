bb6bb5e236b3790e8ea41d651950c038fb716e29
Tobias <tobias@mollstam.com>
2013-06-17 11:03:21 +0200

    Added empty Ec2Instance class and test that actually can load it (some dancing involved)

---
+++ test/TestEc2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import inspect, os, sys
+import boto.ec2
+import mock
+from mock import MagicMock, Mock
+
+# Setup an environment for ec2 module to run in, not the most testable code...
+mock = Mock()
+mock.params = {
+    'wait_timeout': 0,
+    'count': 0,
+}
+AnsibleModule = lambda *args, **kwargs: mock
+BOOLEANS = [True, False]
+for libdir in sys.path:
+    path = os.path.join(libdir, 'cloud/ec2')
+    if (os.path.isfile(path)):
+        execfile(path)
+        break
+
+class TestEc2Instance(unittest.TestCase):
+
+    def test_loaded(self):
+        try:
+            Ec2Instance
+        except NameError:
+            assert False == True, 'Ec2Instance is not loaded'
