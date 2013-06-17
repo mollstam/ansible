9ad218492456ca2607f273490bb2f7b7e512f357
Tobias <tobias@mollstam.com>
2013-06-17 11:35:33 +0200

    Renamed vars/tests for clarity

--- test/TestEc2.py
+++ test/TestEc2.py
@@ -6,13 +6,13 @@ import boto.ec2
 import mock
 from mock import MagicMock, Mock
 
-# Setup an environment for ec2 module to run in, not the most testable code...
-mock = Mock()
-mock.params = {
+# Setup an environment for ec2 module to run in, not the most testable code
+mockedAnsibleModule = Mock()
+mockedAnsibleModule.params = {
     'wait_timeout': 0,
     'count': 0,
 }
-AnsibleModule = lambda *args, **kwargs: mock
+AnsibleModule = lambda *args, **kwargs: mockedAnsibleModule
 BOOLEANS = [True, False]
 for libdir in sys.path:
     path = os.path.join(libdir, 'cloud/ec2')
@@ -22,7 +22,7 @@ for libdir in sys.path:
 
 class TestEc2Instance(unittest.TestCase):
 
-    def test_loaded(self):
+    def test_class_loaded(self):
         try:
             Ec2Instance
         except NameError:
