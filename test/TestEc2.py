db34008f4efb0d3e41c0c6be52580610dfc7eaec
Tobias <tobias@mollstam.com>
2013-06-17 14:52:37 +0200

    Added functionality for testing if a boto instance fulfils our request

--- test/TestEc2.py
+++ test/TestEc2.py
@@ -24,10 +24,30 @@ for libdir in sys.path:
         break
 
 def mocked_instance(params):
-    mocked_instance = Mock(name='mocked_instance')
-    mocked_instance.params = params
-    mocked_instance.tags = params['tags']
+    mocked_instance = Mock(spec_set=params.keys())
+    mocked_instance.configure_mock(**params)
     return mocked_instance
+
+def instance_fulfilling_request(request):
+    mock_group = Mock(name='Group');
+    mock_group.id = request['group_id']
+    mock_group.name = request['group_name']
+    mock_region = Mock(name='RegionInfo');
+    mock_region.name = request['region'];
+    instance = mocked_instance({
+        'key_name': request['key_name'],
+        'groups' : [mock_group],
+        'region': mock_region,
+        'placement': request['zone'],
+        'instance_type': request['instance_type'],
+        'image_id': request['image'],
+        'monitoring_state': 'enabled',
+        'kernel': request['kernel'],
+        'ramdisk': request['ramdisk'],
+        'tags': request['instance_tags'],
+    })
+
+    return instance
 
 class TestEc2IdempotentHandler(unittest.TestCase):
 
@@ -37,8 +57,26 @@ class TestEc2IdempotentHandler(unittest.TestCase):
         self.boto_mock.ec2.connect_ec2.return_value = self.ec2_mock
         ec2_module_scope['boto'] = self.boto_mock
 
+        request = {
+            'key_name': 'test_key_name',
+            'group_name': 'test_group_name',
+            'group_id': 'sg-test',
+            'region': 'test-1',
+            'zone': 'test-1a',
+            'instance_type': 'm1.test',
+            'image': 'ami-test',
+            'monitoring': True,
+            'kernel': 'aki-test',
+            'ramdisk': '-',
+            'instance_tags': {'test-tag-key': 'test-tag-value'},
+        }
         self.ec2_handler = ec2_module_scope['Ec2IdempotentHandler'](
-            ec2_module_scope['boto'].ec2.connect_ec2())
+            ec2_module_scope['boto'].ec2.connect_ec2(), request)
+
+    def assertNotFulfilRequest(self, instance):
+        self.assertEqual(self.ec2_handler.fulfils_request(instance),
+                         False,
+                         'should not fulfil')
 
     def test_class_loaded(self):
         try:
@@ -60,3 +98,63 @@ class TestEc2IdempotentHandler(unittest.TestCase):
             .assert_called_with(None, {'tag-key':'ansible_idempotency_id',
                                        'tag-value': expected_id}))
         self.assertEqual(instances[0], expected_instance)
+
+    def test_instance_fulfils_request_true(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+
+        assert self.ec2_handler.fulfils_request(instance), 'should fulfil'
+
+    def test_instance_fulfils_request_different_key_name(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.key_name = 'another_key_name'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_group_name(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.groups[0].name = 'another_group_name'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_group_id(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.groups[0].id = 'another_group_id'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_region(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.region.name = 'another-1'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_zone(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.placement = 'another-1b'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_instance_type(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.instance_type = 'm1.xanother2'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_image(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.image_id = 'ami-another'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_monitoring(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.monitoring_state = 'disabled'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_kernel(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.kernel = 'aki-another'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_ramdisk(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.ramdisk = 'r-another'
+        self.assertNotFulfilRequest(instance)
+
+    def test_instance_fulfils_request_different_tags(self):
+        instance = instance_fulfilling_request(self.ec2_handler.request)
+        instance.tags = {'Name': 'Another Name'}
+        self.assertNotFulfilRequest(instance)
